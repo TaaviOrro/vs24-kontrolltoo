ca9c8085fec1cd2cc7f6b806d64041b87321aea1
Taavi <taavi.orro@voco.ee>
2025-04-01 11:21:57 +0300

    ulesanne 5 MealItem.js komponendi valuuta valjastamine, toetudes numbrite vormindamise voimalusele

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -5,14 +5,19 @@ const MealItem = ({ name, description, price, image}) => {
     console.log("image prop:", image)
     const imagePath = require(`../assets/${image}`);
 
+    const formattedPrice = new Intl.NumberFormat('de-DE', {
+        style: 'currency',
+        currency: 'EUR',
+    }).format(price);
+
     return (
         <li className='meal-item'>
             <article>
-                <img src={imagePath} alt={name}/>
+                <img className='meal-item-img' src={imagePath} alt={name}/>
                 <div>
                     <h3>{name}</h3>
-                    <p>{description}</p>
-                    <p className="meal-price">{parseFloat(price).toFixed(2)}</p>
+                    <p className="meal-item-description">{description}</p>
+                    <p className="meal-item-price">{formattedPrice}</p>
                 </div>
                 <p>
                     <button >Add to Cart</button>
